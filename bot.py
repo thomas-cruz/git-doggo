688ef4cae8ea55666d09e2612d908268bf7dcbab
thomas-cruz <50308862+thomas-cruz@users.noreply.github.com>
2019-05-22 18:02:52 +0800

    Added try-catch for duplicate tweets and set interval to 1 hour

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import time
 import sys
 import tweepy
 import requests
-from requests.auth import HTTPDigestAuth
 import json
 
 
@@ -12,27 +11,30 @@ ACCESS_KEY = ''
 ACCESS_SECRET = ''
 
 
-# INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-INTERVAL = 15  # every 15 seconds, for testing
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+Iauth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-
-
-url = 'https://api.github.com/search/commits?q=doggo&sort=committer-date'
-response = requests.get(url, headers = {"Accept": "application/vnd.github.cloak-preview"}, verify=True)
-parsed_json = json.loads(response.text)
 message_index = 29
-prev_commit = parsed_json['items'][message_index]['commit']['message']
+
 while True:
-	commit = parsed_json['items'][message_index]['commit']['message']
-	if(commit == prev_commit):
-		message_index = message_index - 1
-		commit = parsed_json['items'][message_index]['commit']['message']
-	print(commit)
-	print (len(parsed_json['items']))
-	print("about to create status...")
-	ad = commit
-	prev_commit = commit
-	api.update_status(ad)
-	time.sleep(INTERVAL)
+    url = 'https://api.github.com/search/commits?q=doggo&sort=committer-date'
+    response = requests.get(url, headers = {"Accept": "application/vnd.github.cloak-preview"}, verify=True)
+    parsed_json = json.loads(response.text)
+    prev_commit = parsed_json['items'][message_index]['commit']['message']
+    while message_index > 0:
+    	commit = parsed_json['items'][message_index]['commit']['message']
+    	if(commit == prev_commit):
+    		message_index = message_index - 1
+    		commit = parsed_json['items'][message_index]['commit']['message']
+    	print(commit)
+    	print (len(parsed_json['items']))
+    	print("about to create status...")
+    	ad = commit
+    	prev_commit = commit
+    	try:
+    	    api.update_status(ad)
+    	    time.sleep(INTERVAL)
+    	except:
+    	    message_index = message_index - 1
+    	    break
